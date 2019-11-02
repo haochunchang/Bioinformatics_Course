97596808db2cde3d59b2cc053d464cc99d3076bb
haochunchang <jr55662013@gmail.com>
2019-11-02 21:17:11 +0800

    add R intro and Dataframe

---
+++ basics/R/exercise.R
@@ -0,0 +1,17 @@
+# Question one -- Fibonacci Sequence (6 %)
+fibonacci_sequence <- function(n) { # n means the Nth number in Fibonacci sequence
+    answer <- 0 # You have to asign your answer into this variable
+    # YOUR CODE
+
+    
+    
+    return(answer)
+}
+
+# Question two -- Diamond Shape (14 %)
+diamond_shape <- function(n) {
+    # YOUR CODE
+    
+    
+    
+}
