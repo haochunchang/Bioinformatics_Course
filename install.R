131d56ebfe974387e9c37afbc7524069a164f56f
haochunchang <jr55662013@gmail.com>
2020-05-01 23:49:22 +0800

    Add jupyter notebook in R for DEG.
    Add binder link
    
    Also change license to MIT.

---
+++ install.R
@@ -0,0 +1,9 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("limma")
+BiocManager::install("edgeR")
+
+install.packages("RColorBrewer")
+install.packages("ggplot2")
+install.packages("ggrepel")
+install.packages("statmod")
